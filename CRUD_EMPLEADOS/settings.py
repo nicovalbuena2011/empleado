e84674e4ea2ff7dad205d151c69e851ce733a8e3
nicovalbuena2011 <nicovalbuena2011@gmail.com>
2023-09-24 17:14:27 -0500

    Se comento la ruta de archivos estaticos y se agrego otra y se compilaron

--- CRUD_EMPLEADOS/settings.py
+++ CRUD_EMPLEADOS/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_k')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['empleados-proyecto01.azurewebsites.net','localhost','http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['prueba-empleados.azurewebsites.net','localhost','http://127.0.0.1:8000/']
 
 
 # Application definition
@@ -142,7 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
