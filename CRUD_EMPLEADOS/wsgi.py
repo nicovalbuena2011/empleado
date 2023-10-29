bb6552f5929192fc6654c801ef202718dc4abc44
nicovalbuena2011 <nicovalbuena2011@gmail.com>
2023-10-28 21:48:47 -0500

    se agrego el app en el archivo wsgi

--- CRUD_EMPLEADOS/wsgi.py
+++ CRUD_EMPLEADOS/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRUD_EMPLEADOS.settings')
 
 application = get_wsgi_application()
+
+app = application
